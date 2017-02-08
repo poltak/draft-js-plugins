7d166a950366956bf8eb06cf0c55424899e81be6
Eryk Napierała <eryk.piast@gmail.com>
2017-02-08 20:00:37 +0100

    improve toolbar positioning

---
+++ draft-js-inline-toolbar-plugin/src/components/Toolbar/getPosition.js
@@ -0,0 +1,22 @@
+function getSingleDimension(sizeProp, positionProp) {
+  return ({ selection, parent, scroll, toolbar }) => {
+    const halfSelectionSize = selection[sizeProp] / 2;
+    const halfToolbarSize = toolbar[sizeProp] / 2;
+    const parentPosition = parent[positionProp] + scroll[positionProp];
+    const selectionPosition = selection[positionProp] + scroll[positionProp];
+    const selectionMiddle = (selectionPosition - parentPosition) + halfSelectionSize;
+    const horizontalOffset = Math.max(0, (selectionMiddle + halfToolbarSize) - parent[sizeProp]);
+
+    return Math.max(0, selectionMiddle - halfToolbarSize - horizontalOffset);
+  };
+}
+
+const getLeft = getSingleDimension('width', 'left');
+const getTop = getSingleDimension('height', 'top');
+
+export default function getToolbarPosition(params) {
+  return {
+    left: getLeft(params),
+    top: getTop(params),
+  };
+}
--- draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
+++ draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
 import { getVisibleSelectionRect } from 'draft-js';
 
-// TODO make toolbarHeight to be determined or a parameter
-const toolbarHeight = 44;
+import getToolbarPosition from './getPosition';
 
-export default class Toolbar extends React.Component {
+
+export default class Toolbar extends Component {
+  static propTypes = {
+    store: PropTypes.shape({
+      getItem: PropTypes.func.isRequired, // eslint-disable-line react/no-unused-prop-types
+      subscribeToItem: PropTypes.func.isRequired,
+      unsubscribeFromItem: PropTypes.func.isRequired,
+    }).isRequired,
+    structure: PropTypes.arrayOf(PropTypes.func).isRequired,
+    theme: PropTypes.shape({
+      buttonStyles: PropTypes.string.isRequired, // eslint-disable-line react/no-unused-prop-types
+      toolbarStyles: PropTypes.shape({
+        toolbar: PropTypes.string.isRequired, // eslint-disable-line react/no-unused-prop-types
+      }).isRequired,
+    }).isRequired,
+  }
 
   state = {
     isVisible: false,
+    style: {
+      transform: 'scale(0)',
+    },
   }
 
   componentWillMount() {
     this.props.store.subscribeToItem('isVisible', this.onVisibilityChanged);
+  }
+
+  componentDidMount() {
+    this.node = findDOMNode(this);
   }
 
   componentWillUnmount() {
@@ -19,40 +41,60 @@ export default class Toolbar extends React.Component {
   }
 
   onVisibilityChanged = (isVisible) => {
+    if (!isVisible) {
+      this.setState({
+        style: {
+          transform: 'scale(0)',
+        },
+      });
+      return;
+    }
+
+    // NOTE: forced layout calculation!
+    const prevTransform = this.node.style.transform;
+    const prevTransition = this.node.style.transition;
+    this.node.style.transform = 'scale(1)';
+    this.node.style.transition = 'none';
+    const toolbarRect = this.node.getBoundingClientRect();
+    const parentRect = this.node.offsetParent.getBoundingClientRect();
+    this.node.style.transform = prevTransform;
+    this.node.style.transition = prevTransition;
+
     // need to wait a tick for window.getSelection() to be accurate
     // when focusing editor with already present selection
     setTimeout(() => {
-      const selectionRect = isVisible ? getVisibleSelectionRect(window) : undefined;
-      const position = selectionRect ? {
-        top: (selectionRect.top + window.scrollY) - toolbarHeight,
-        left: selectionRect.left + window.scrollX + (selectionRect.width / 2),
-        transform: 'translate(-50%) scale(1)',
-        transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
-      } : {
-        transform: 'translate(-50%) scale(0)',
-      };
+      const selectionRect = getVisibleSelectionRect(window);
+      const { top, left } = getToolbarPosition({
+        parent: parentRect,
+        selection: selectionRect,
+        scroll: {
+          left: window.scrollX,
+          top: window.scrollY,
+        },
+        toolbar: toolbarRect,
+      });
+
       this.setState({
-        position,
+        style: {
+          left,
+          top,
+          transform: 'scale(1)',
+          transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
+        },
       });
     }, 0);
   }
 
   render() {
-    const { theme, store } = this.props;
-    return (
-      <div
-        className={theme.toolbarStyles.toolbar}
-        style={this.state.position}
-      >
-        {this.props.structure.map((Component, index) => (
-          <Component
-            key={index}
-            theme={theme.buttonStyles}
-            getEditorState={store.getItem('getEditorState')}
-            setEditorState={store.getItem('setEditorState')}
-          />
-        ))}
-      </div>
-    );
+    const { store, structure, theme } = this.props;
+    return (<div
+      className={theme.toolbarStyles.toolbar}
+      style={this.state.position}
+    >{structure.map((Element, index) => (<Element
+      key={index}
+      theme={theme.buttonStyles}
+      getEditorState={store.getItem('getEditorState')}
+      setEditorState={store.getItem('setEditorState')}
+    />))}</div>);
   }
 }
